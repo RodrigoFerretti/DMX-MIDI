b0b5d4cef485747c59651e55c35c2aa9967b6336
RodrigoFerretti <rodrigo_ferretti@hotmail.com>
2025-04-02 15:41:50 -0300

    indenting

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,28 @@
 #include <USB-MIDI.h>
 
 #define ON_LED_PIN 13
+
 #define DMX_TX_PIN 1
 #define DMX_RX_PIN 0
+
 #define MIDI_CHANNEL MIDI_CHANNEL_OMNI
+
 #undef DMXSERIAL_MAX
 #define DMXSERIAL_MAX 12
 
 typedef USBMIDI_NAMESPACE::usbMidiTransport _umt;
-typedef MIDI_NAMESPACE::MidiInterface<_umt> _ss;
+
+typedef MIDI_NAMESPACE::MidiInterface<_umt> _mi;
 
 _umt usbMIDI(0);
-_ss MIDICoreUSB((_umt &)usbMIDI);
+
+_mi MIDICoreUSB((_umt &)usbMIDI);
 
 uint8_t prevDmxValues[DMXSERIAL_MAX + 1];
 
 static void onNoteOn(byte channel, byte note, byte velocity)
 {
-  if (note > 0 && note <= DMXSERIAL_MAX)
+  if (note <= DMXSERIAL_MAX && note > 0)
   {
     DmxSimple.write(note, (velocity - 1) * 2);
   }
@@ -28,6 +33,7 @@ static void onNoteOn(byte channel, byte note, byte velocity)
 void setup()
 {
   Serial.begin(115200);
+
   pinMode(ON_LED_PIN, OUTPUT);
   digitalWrite(ON_LED_PIN, HIGH);
 
@@ -64,6 +70,7 @@ void loop()
 
       MIDICoreUSB.sendNoteOn(midiNote, velocity, 1);
       MIDICoreUSB.sendNoteOff(midiNote, 0, 1);
+
       prevDmxValues[channel] = dmxValue;
     }
   }
